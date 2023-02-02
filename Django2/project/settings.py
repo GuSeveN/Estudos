dbb7bed84edb31e380a8e1aabbe2d0c8e6405911
Gustavo Alvetti <102641516+GuSeveN@users.noreply.github.com>
2023-02-02 14:26:21 -0300

    Django - FUCTURA
    Projeto inicial, aprendizagem no Django

--- Django2/project/settings.py
+++ Django2/project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*0m=4%7%c%w*tdbi)11%gog61e7u*e+y&a&5w4&ji-$$(_lw_w'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
