90588d291a4948cee6aaed37da1399b9af3cffba
Gustavo Alvetti <102641516+GuSeveN@users.noreply.github.com>
2023-02-14 16:39:19 -0300

    Update Aula_67.py
    Calculadora basica

--- Curso_udemy/Aula_67.py
+++ Curso_udemy/Aula_67.py
@@ -2,20 +2,20 @@
 
 while True:
     print('**** Calculadora ****')
-    numero = print(float(input('Digite um numero: ').strip()))
-    numero2 = print(float(input('Digite outro numero: ').strip()))
-    mat = str(print(input(
-        'Quer \n somar [+] \n subtrair [-] \n Mutiplicar [x] \n Dividir [/] : ').strip()))
-    if mat == "+":
+    numero = float (input ('Digite um numero: ').strip())
+    numero2 = float (input ('Digite outro numero: ').strip())
+    mat = str (input ('Digite um operador \n + = Soma \n - = Subtracao \n x = Mutiplicacao \n / = Divicao \n').strip())
+    if mat =='+':
         print(f'A soma é {numero+numero2}')
-    elif mat == '-':
+    elif mat =='-':
         print(f'A subtracão é {numero-numero2}')
-    elif mat == 'x':
+    elif mat =='x':
         print(f'Mutiplicacão é {numero*numero2}')
-    elif mat == '/':
+    elif mat =='/':
         print(f'Divisão é {numero/numero2}')
     else:
         print('Não selecionou corretamente')
+        continue
 
     print("1 para SAIR")
     if int(input()) == 1:
